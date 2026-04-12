729c1be0777f300e09432b7b28ce1d2c25e38f6b
oldful-dev <developer@oldful.com>
2026-04-12 20:44:11 +0530

    Cleanup: Remove scratch scripts from tracking and update .gitignore

--- mobile/check1.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const dir = 'd:\\codes\\MERN\\medico\\mobile\\assets\\images';
-const file = 'd8ad60edd50d15bfa3472e8a2d9ca46b49e1d6b3.png';
-const buf = fs.readFileSync(path.join(dir, file));
-const hex = Array.from(buf.slice(0, 16)).map(b => b.toString(16).padStart(2, '0')).join(' ');
-console.log(file + ': ' + hex + ' size=' + buf.length);
--- read_raw.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('D:\\codes\\MERN\\medico\\OLDFUL API FILE.pdf');
-console.log(content.toString('utf8').slice(0, 2000));
